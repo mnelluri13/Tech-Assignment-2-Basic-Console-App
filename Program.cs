939415562b37090190cd72fa97e07a7c029b31ea
Meghana Nelluri <mnelluri@usf.edu>
2022-01-28 18:53:51 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+//Name: Meghana Nelluri
+//Project: Tech Assignment 2
+//Date: 1/27/22
+namespace Tech_Assignment_2_Basic_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My name is Meghana Nelluri and I am currently reading a play called Waiting for Godot by Samuel Beckett.");
+        }
+    }
+}
