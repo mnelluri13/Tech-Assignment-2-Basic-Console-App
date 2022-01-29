7bfa5680396922537c278dba2ea2b0a4a9888fdf
Meghana Nelluri <mnelluri@usf.edu>
2022-01-28 19:06:23 -0500

    Commit change

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-//Name: Meghana Nelluri
-//Project: Tech Assignment 2
+//Programmer Name: Meghana Nelluri
+//Project: Tech Assignment 2-Basic Console App
 //Date: 1/27/22
 namespace Tech_Assignment_2_Basic_Console_App
 {
@@ -8,6 +8,7 @@ namespace Tech_Assignment_2_Basic_Console_App
     {
         static void Main(string[] args)
         {
+            //Write console message about me
             Console.WriteLine("My name is Meghana Nelluri and I am currently reading a play called Waiting for Godot by Samuel Beckett.");
         }
     }
